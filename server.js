717dc28870f3bc4c00e92401a8f8ffba795a7b50
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-06-02 13:13:32 +0530

    port changes

--- server.js
+++ server.js
@@ -67,5 +67,5 @@ app.use("/api/blog", blogRoutes);
 
 
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5006;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
